a44e6ef080782661929f00253b2900e2b3a6b7f7
MS10306-TCHER <wanpracha.n@rmutsv.ac.th>
2019-09-11 11:54:45 +0700

    Hello World

---
+++ android/app/src/main/kotlin/th/ac/rmutsv/rattaphum/iot/MainActivity.kt
@@ -0,0 +1,13 @@
+package th.ac.rmutsv.rattaphum.iot
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
